d535b0c3f6270a392d2817998cad684ce6d865b5
Vhen Larson A. Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2020-12-21 19:37:48 +0800

    Added extra validations

--- src/app/components/credit-card/credit-card-payment/credit-card-payment.component.ts
+++ src/app/components/credit-card/credit-card-payment/credit-card-payment.component.ts
@@ -17,17 +17,59 @@ export class CreditCardPaymentComponent implements OnInit {
   }
 
   initForm(): void {
+    const allowDigitOnly = /^[0-9]\d*$/;
+
     this.paymentForm = new FormGroup({
       creditCardNumber: new FormControl(null, Validators.required),
       cardHolder: new FormControl(null, Validators.required),
-      expirationMonth: new FormControl(null, [Validators.required, Validators.maxLength(2)]),
-      expirationYear: new FormControl(null, [Validators.required, Validators.maxLength(2)]),
-      securityCode: new FormControl(null, Validators.maxLength(3)),
-      amount: new FormControl(null, [Validators.required,  Validators.pattern(/^[1-9]+[0-9]*$/)])
+      expirationMonth: new FormControl(
+        null,
+        [
+          Validators.required,
+          Validators.pattern(allowDigitOnly),
+          this.monthValidation.bind(this),
+          // this.expirationValidation.bind(this)
+        ]),
+      expirationYear: new FormControl(null, [Validators.required, Validators.pattern(allowDigitOnly)]),
+      securityCode: new FormControl(
+        null,
+        [
+          Validators.required,
+          Validators.pattern(allowDigitOnly),
+          Validators.minLength(3)
+        ]
+      ),
+      amount: new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
     });
   }
 
+  monthValidation(control: FormControl): {[s: string]: boolean} {
+    if (control.value > 12 || +control.value === 0 || control.value === '00') {
+      return {monthExceeded: true};
+    }
+    return null;
+  }
+
+
+
+  expirationValidation(): {[s: string]: boolean} {
+
+    if (this.paymentForm.get('expirationMonth').value) {
+      return;
+    }
+    // const month = '12';
+    // const year = this.paymentForm.get('expirationYear').value;
+    const currentMonth = new Date().getMonth() + 1;
+    const currentYear = new Date().getFullYear().toString().substr(-2);
+    // if (+month <= currentMonth) {
+    //   return {expired: true};
+    // }
+  }
+
   onSubmit(): void {
-    console.log(this.paymentForm.value);
+    // if (!this.paymentForm.valid) {
+    //   return;
+    // }
+    console.log();
   }
 }
--- src/app/models/credit-card-payment.model.ts
+++ src/app/models/credit-card-payment.model.ts
@@ -2,6 +2,7 @@ export interface CreditCardPayment {
     creditCardNumber: string;
     cardHolder: string;
     expirationDate: Date;
-    securityCode?: string;
+    // securityCode?: string;
+    securityCode: string; // I changed to mandoratory because CCV is require in every payment.
     amount: number;
 }
