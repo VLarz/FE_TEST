d00cdce2e48b849e2e65b6367c0123d6499d039c
Vhen Larson A. Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2020-12-21 15:27:42 +0800

    Done adding reducer and actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { creditCardPaymentReducer } from './components/store/credit-card-payment.reducer';
+import { CreditCardPaymentComponent } from './components/credit-card-payment/credit-card-payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreditCardPaymentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({creditCardPayment: creditCardPaymentReducer}),
   ],
   providers: [],
   bootstrap: [AppComponent]
