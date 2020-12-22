f2b1954a4e5e396d1b7847f431fdfd7d49972aca
Vhen Larson A. Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2020-12-22 16:01:27 +0800

    Changed initial state date value

--- src/app/store/credit-card-payment.reducer.ts
+++ src/app/store/credit-card-payment.reducer.ts
@@ -2,9 +2,9 @@ import { CreditCardPayment } from '../models/credit-card-payment.model';
 import * as PaymentActions from './credit-card-payment.actions';
 
 const initialState: CreditCardPayment = {
-  creditCardNumber: '379335307626997',
+  creditCardNumber: '3793 3530 7626 9973',
   cardHolder: 'Vhen Larson Dela Cuesta',
-  expirationDate: new Date(Date.now()),
+  expirationDate: new Date(new Date().setFullYear(2021, 11, 1)),
   securityCode: '416',
   amount: 50000
 };
