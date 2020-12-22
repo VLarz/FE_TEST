455f284400c659646aadbfc7e1d1de5a00021370
Vhen Larson A. Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2020-12-22 14:42:07 +0800

    Added action

---
+++ src/app/store/credit-card-payment.actions.ts
@@ -0,0 +1,13 @@
+import { Action } from '@ngrx/store';
+
+import { CreditCardPayment } from '../models/credit-card-payment.model';
+
+export const ADD_PAYMENT = '[CREDIT CARD PAYMENT] ADD_PAYMENT';
+
+export class AddPayment implements Action {
+  readonly type = ADD_PAYMENT;
+
+  constructor(public payload: CreditCardPayment) {}
+}
+
+export type Actions = AddPayment;
