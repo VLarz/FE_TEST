6856a986b01cc001a43bfe9f83a5912938dc86fa
Vhen Larson A. Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2020-12-22 14:43:40 +0800

    Added service

--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+
+import { CreditCardPayment } from '../models/credit-card-payment.model';
+import { environment } from '../../environments/environment';
+import { ENDPOINT } from '../const/endpoints.const';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  storePaymentRecords(record: CreditCardPayment): Observable<CreditCardPayment> {
+    return this.http
+      .post<CreditCardPayment>(
+        `${environment.apiUrl}${ENDPOINT.PAYMENT_RECORDS}`,
+        record
+      );
+  }
 }
